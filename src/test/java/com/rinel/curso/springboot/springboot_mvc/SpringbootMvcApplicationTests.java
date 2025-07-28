84d5e0157597f72d2395754bb89765e6c4c480c4
Rinel <rineliniguezsosa@gmail.com>
2025-07-28 15:56:07 -0600

    [FEAT] Inicializando el proyecto spring boot mvc

---
+++ src/test/java/com/rinel/curso/springboot/springboot_mvc/SpringbootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rinel.curso.springboot.springboot_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
