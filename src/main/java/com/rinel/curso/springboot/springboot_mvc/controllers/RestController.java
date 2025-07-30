c1ddc084293df80bd1cc0c6ea347d506ed4cbdc9
Rinel <rineliniguezsosa@gmail.com>
2025-07-30 15:21:39 -0600

    [FEAT] Creando el archivo RestController.java es una copia de UserController.java

---
+++ src/main/java/com/rinel/curso/springboot/springboot_mvc/controllers/RestController.java
@@ -0,0 +1,18 @@
+package com.rinel.curso.springboot.springboot_mvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RestController {
+
+    // parametros en la vista
+    @GetMapping("/details")
+    public String detailString(Model model) {
+        model.addAttribute("title", "Hola mundo spring  boot");
+        model.addAttribute("name", "Rinel");
+        return "Detalles";
+    }
+    
+}
