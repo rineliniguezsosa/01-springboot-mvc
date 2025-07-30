7815c431a042422c15ef7deae87d1b6f4843bf1e
Rinel <rineliniguezsosa@gmail.com>
2025-07-30 15:15:53 -0600

    [FEAT] Documentando el método detailString con un comentario de linea

--- src/main/java/com/rinel/curso/springboot/springboot_mvc/controllers/UserController.java
+++ src/main/java/com/rinel/curso/springboot/springboot_mvc/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
+    // parametros en la vista
     @GetMapping("/details")
     public String detailString(Model model) {
         model.addAttribute("title", "Hola mundo spring  boot");
