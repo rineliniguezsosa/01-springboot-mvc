08cf2c6fb6f68b1c1e459e392f20e8ce12ae28b5
Rinel <rineliniguezsosa@gmail.com>
2025-07-30 11:44:53 -0600

    [FEAT] Creando el método detailString
    - importando el método getmapping para usar la solicitud get
    - creando el método detailString el cual retorna un string

--- src/main/java/com/rinel/curso/springboot/springboot_mvc/controllers/UserController.java
+++ src/main/java/com/rinel/curso/springboot/springboot_mvc/controllers/UserController.java
@@ -1,8 +1,14 @@
 package com.rinel.curso.springboot.springboot_mvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
+    @GetMapping("/details")
+    public String detailString() {
+        return "Detalles";
+    }
+    
 }
