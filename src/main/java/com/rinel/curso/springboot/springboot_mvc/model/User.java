0e52d644e015c340f2e9274d30f133ed8f25cdee
Rinel <rineliniguezsosa@gmail.com>
2025-07-31 15:22:37 -0600

    [FEAT] Cambios en el modelo User
    - creando las props privadas name y lastname
    - creando el constructor User
    - creando los métodos getter y setter

--- src/main/java/com/rinel/curso/springboot/springboot_mvc/model/User.java
+++ src/main/java/com/rinel/curso/springboot/springboot_mvc/model/User.java
@@ -1,5 +1,26 @@
 package com.rinel.curso.springboot.springboot_mvc.model;
 
 public class User {
+    private String name;
+    private String lastname;
+    
+    public User(String name, String lastname) {
+        this.name = name;
+        this.lastname = lastname;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 
+    
 }
