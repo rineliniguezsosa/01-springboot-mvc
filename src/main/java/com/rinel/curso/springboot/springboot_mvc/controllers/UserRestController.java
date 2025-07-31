f7a6535a83fb50dfb350d8ac6d326e4e1127ac9c
Rinel <rineliniguezsosa@gmail.com>
2025-07-31 17:28:28 -0600

    [FEAT] Cambios en los métodos detailString y detailMap
    - importando UserDTO
    - creando una copia de la ruta details2
    - modificamos el método detailString en donde ahora devolveremos la data de userdto

--- src/main/java/com/rinel/curso/springboot/springboot_mvc/controllers/UserRestController.java
+++ src/main/java/com/rinel/curso/springboot/springboot_mvc/controllers/UserRestController.java
@@ -10,15 +10,29 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rinel.curso.springboot.springboot_mvc.model.User;
+import com.rinel.curso.springboot.springboot_mvc.model.dto.UserDTO;
 
 @RestController
 @RequestMapping("/usuario")
 public class UserRestController {
 
     // salida en formato json
-    @GetMapping("/details2")
+    @GetMapping("/details")
     @ResponseBody
-    public Map<String,Object> detailString(Model model) {
+    public UserDTO detailString(Model model) {
+        UserDTO userdto = new UserDTO();
+
+        User user = new User("rinel","iñiguez");
+
+        userdto.setUser(user);
+        userdto.setTitle("Hola mundo spring boot");
+       
+        return userdto;
+    }
+
+    @GetMapping("/details2-map")
+    @ResponseBody
+    public Map<String,Object> detailMap(Model model) {
         User user = new User("rinel","iñiguez");
         Map<String,Object> body = new HashMap<>();
         body.put("title", "Hola mundo spring  boot");
