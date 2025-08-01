11d77b1636671ae39ace91974aa62004768bfad4
Rinel <rineliniguezsosa@gmail.com>
2025-08-01 13:47:23 -0600

    [FEAT] Estableciendo los métodos getter y setter y la prop email en el contructor en la entidad User

--- src/main/java/com/rinel/curso/springboot/springboot_mvc/model/User.java
+++ src/main/java/com/rinel/curso/springboot/springboot_mvc/model/User.java
@@ -3,10 +3,12 @@ package com.rinel.curso.springboot.springboot_mvc.model;
 public class User {
     private String name;
     private String lastname;
+    private String email;
     
-    public User(String name, String lastname) {
+    public User(String name, String lastname, String email) {
         this.name = name;
         this.lastname = lastname;
+        this.email = email;
     }
     
     public String getName() {
@@ -22,5 +24,14 @@ public class User {
         this.lastname = lastname;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
     
 }
