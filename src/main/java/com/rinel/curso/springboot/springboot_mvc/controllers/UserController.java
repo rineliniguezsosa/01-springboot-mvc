2aa5a6ae616b657eb8c7e976ac7347522095fe6b
Rinel <rineliniguezsosa@gmail.com>
2025-08-01 16:26:37 -0600

    [FEAT] Creando el método list en la clase UserController
    - importando Arrays
    - importando List
    - importando ModelMap
    - creando el método list con la ruta list donde mostramos la vista templates2
    - pasando como argumento ModelMap
    - creé una lista de tipo User en donde creé 2 instancias
    - establecemos las instancias al template

--- src/main/java/com/rinel/curso/springboot/springboot_mvc/controllers/UserController.java
+++ src/main/java/com/rinel/curso/springboot/springboot_mvc/controllers/UserController.java
@@ -1,8 +1,15 @@
 package com.rinel.curso.springboot.springboot_mvc.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.rinel.curso.springboot.springboot_mvc.model.User;
+
 
 @Controller
 public class UserController {
@@ -13,6 +20,18 @@ public class UserController {
         model.addAttribute("title", "Hola mundo spring  boot");
         model.addAttribute("name", "Rinel");
         return "Detalles";
+    }
+    
+
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+        List<User> users = Arrays.asList(
+            new User("rinel", "sosa", "rineliniguezsosa@gmail.com"),
+            new User("merly", "sosa", "merlysosa@gmail.com")
+        );
+
+        model.addAttribute(users);
+        return "templates2";
     }
     
 }
