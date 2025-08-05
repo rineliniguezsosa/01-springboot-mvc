f499c764367bb3195de7546b6f0bc84ed88af8ab
Rinel <rineliniguezsosa@gmail.com>
2025-08-05 16:50:06 -0600

    [FEAT] Estableciendo el decorador requestmapping para definir una url base el cual es /api/params

--- src/main/java/com/rinel/curso/springboot/springboot_mvc/controllers/RequestParamController.java
+++ src/main/java/com/rinel/curso/springboot/springboot_mvc/controllers/RequestParamController.java
@@ -1,5 +1,8 @@
 package com.rinel.curso.springboot.springboot_mvc.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/params")
 public class RequestParamController {
 
 }
