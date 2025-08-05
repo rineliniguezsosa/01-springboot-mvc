09c2d08ed2b368a96aee22eb3cbccdcb7cee9cc1
Rinel <rineliniguezsosa@gmail.com>
2025-08-05 17:33:45 -0600

    [FEAT] Creando el método foo  para establecer los parametros
    - importando el decorador GetMapping
    - importando el decorador RequestParam
    - importando la clase Paramdto
    - creando el método y la ruta "foo"
    - creando una instancia de Paramdto en donde pasamos el param mesage
    - retornamos el param

--- src/main/java/com/rinel/curso/springboot/springboot_mvc/controllers/RequestParamController.java
+++ src/main/java/com/rinel/curso/springboot/springboot_mvc/controllers/RequestParamController.java
@@ -1,8 +1,20 @@
 package com.rinel.curso.springboot.springboot_mvc.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.rinel.curso.springboot.springboot_mvc.model.dto.Paramdto;
 
 @RequestMapping("/api/params")
 public class RequestParamController {
+
+
+    @GetMapping("/foo")
+    public Paramdto foo(@RequestParam String message){
+        Paramdto param = new Paramdto(message);
+
+        return param;
+    }
 
 }
