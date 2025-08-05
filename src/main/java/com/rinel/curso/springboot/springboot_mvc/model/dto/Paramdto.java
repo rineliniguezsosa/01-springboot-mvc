8b99eff2b7c24087f171287186435a4b27b74e58
Rinel <rineliniguezsosa@gmail.com>
2025-08-05 17:23:19 -0600

    [FEAT] Creando los métodos getter y setter de la propiedade message y el constructor de Paramdto para crear instancias de tipo message

--- src/main/java/com/rinel/curso/springboot/springboot_mvc/model/dto/Paramdto.java
+++ src/main/java/com/rinel/curso/springboot/springboot_mvc/model/dto/Paramdto.java
@@ -2,4 +2,19 @@ package com.rinel.curso.springboot.springboot_mvc.model.dto;
 
 public class Paramdto {
 
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+    
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Paramdto(String message) {
+        this.message = message;
+    }
+    
+    
 }
