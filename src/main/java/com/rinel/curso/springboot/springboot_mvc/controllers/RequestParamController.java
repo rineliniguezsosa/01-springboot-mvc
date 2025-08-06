9e91b3707674e8cd4fc042a3e1d10d3298a56636
Rinel <rineliniguezsosa@gmail.com>
2025-08-06 11:17:52 -0600

    [FEAT] Cambios en la clase RequestParamController
    - importando el decorador RestController
    - estableciendo las props required y defaultValue
    - eliminando la prop message en la creación de la instancia Paramdto
    - estableciendo el valor de message en setter setMessage

--- src/main/java/com/rinel/curso/springboot/springboot_mvc/controllers/RequestParamController.java
+++ src/main/java/com/rinel/curso/springboot/springboot_mvc/controllers/RequestParamController.java
@@ -3,17 +3,19 @@ package com.rinel.curso.springboot.springboot_mvc.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.rinel.curso.springboot.springboot_mvc.model.dto.Paramdto;
 
+@RestController
 @RequestMapping("/api/params")
 public class RequestParamController {
 
 
     @GetMapping("/foo")
-    public Paramdto foo(@RequestParam String message){
-        Paramdto param = new Paramdto(message);
-
+    public Paramdto foo(@RequestParam(required = false, defaultValue = "Hola mundo") String message){
+        Paramdto param = new Paramdto();
+        param.setMessage(message);
         return param;
     }
 
