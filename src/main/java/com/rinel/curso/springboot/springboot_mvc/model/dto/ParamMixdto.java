345c1673fbb0429b69b0610a43d7526a6f58c738
Rinel <rineliniguezsosa@gmail.com>
2025-08-06 11:46:35 -0600

    [FEAT] Creando la clase ParamMixdto en dode usaremos esta clase para los multiples parametros

---
+++ src/main/java/com/rinel/curso/springboot/springboot_mvc/model/dto/ParamMixdto.java
@@ -0,0 +1,22 @@
+package com.rinel.curso.springboot.springboot_mvc.model.dto;
+
+public class ParamMixdto {
+     private String message;
+    private Integer code;
+
+    public String getMessage() {
+        return message;
+    }
+    
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+}
