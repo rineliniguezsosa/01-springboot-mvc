642dcb4985ba1ce95934da5b4a1d79a8ecaa0d18
Rinel <rineliniguezsosa@gmail.com>
2026-01-16 14:02:08 -0600

    [FEAT] Multiples cambios en RequestParamController
    - importando el objeto HttpServletRequest para obtener los parametros
    - use el decorador getMapping para establecer la solicitud de tipo get, la ruta tiene como nombre "request"
    - estableciendoel método publico el cual retornamos ParamMixdto, nombre del método request
    - establecemos el parametro request con el objeto HttpServletRequest
    - creamos la variable code igual a 10 de tipo Integer, esta variable sirve para devolver el code si no se envia como parametro
    -estableciendo un try catch para validar el valor de code
    - actualizamos el valor de code por el parametro que obtenemos mediante el objeto HttpServletRequest y lo convertimos a entero
    - si no llega no hacemos nada
    - hacemos una instancia de ParamMixdto ya qye es lo que esperamos devolver
    - usamos los métodos setter para establecer los valores de los parametros a los de la instancia param
    - devolvemos la instancia param

--- src/main/java/com/rinel/curso/springboot/springboot_mvc/controllers/RequestParamController.java
+++ src/main/java/com/rinel/curso/springboot/springboot_mvc/controllers/RequestParamController.java
@@ -1,5 +1,7 @@
 package com.rinel.curso.springboot.springboot_mvc.controllers;
 
+import java.net.http.HttpRequest;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.rinel.curso.springboot.springboot_mvc.model.dto.ParamMixdto;
 import com.rinel.curso.springboot.springboot_mvc.model.dto.Paramdto;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api/params")
@@ -26,6 +30,20 @@ public class RequestParamController {
         ParamMixdto param = new ParamMixdto();
         param.setMessage(txt);
         param.setCode(code);
+        return param;
+    }
+
+    @GetMapping("/request")
+    public ParamMixdto request(HttpServletRequest request){
+        Integer code = 10;
+        try {
+            code = Integer.parseInt(request.getParameter("code"));
+        } catch (NumberFormatException e) {
+            // TODO: handle exception
+        }
+        ParamMixdto param = new ParamMixdto();
+        param.setCode(code);
+        param.setMessage(request.getParameter("txt"));
         return param;
     }
 
