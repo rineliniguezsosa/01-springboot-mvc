703fa1747ad7ffffd28b49b3284fbe4db7496d61
Rinel <rineliniguezsosa@gmail.com>
2026-01-16 15:30:35 -0600

    [FEAT] Multiples cambios en PathVariableController
    - importando RequestMapping, RestController, Paramdto, GetMapping y PathVariable
    - estableciendo RestController para definir solicitudes http
    - usando RequestMapping para definir mi url principal "/api/path"
    - definiendo el método de tipo get con el decorador GetMapping en donde accederemos a el mediante la ruta "/var/{message}"
    - message es la varoable que obtendremos de la ruta
    - creando el método var en donde devolveremos Paramdto
    - definimos el parametro message de tipo string el cual lo obtendremos el decorador @PathVariable
    - creamos una instancia de Paramdto
    - establecemos el parametro message a la instancia de Paramdto
    - devolvemos param

--- src/main/java/com/rinel/curso/springboot/springboot_mvc/controllers/PathVariableController.java
+++ src/main/java/com/rinel/curso/springboot/springboot_mvc/controllers/PathVariableController.java
@@ -1,5 +1,24 @@
 package com.rinel.curso.springboot.springboot_mvc.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rinel.curso.springboot.springboot_mvc.model.dto.Paramdto;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@RestController
+@RequestMapping("/api/path")
 public class PathVariableController {
+
+    @GetMapping("/var/{message}")
+    public Paramdto var(@PathVariable String message) {
+        Paramdto param = new Paramdto();
+        param.setMessage(message);
+        return param;
+    }
+    
     
 }
