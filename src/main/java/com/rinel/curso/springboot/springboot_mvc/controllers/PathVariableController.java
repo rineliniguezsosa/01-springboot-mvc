d7b320b88bc4c6dce77e2261d514ec722ad68698
Rinel <rineliniguezsosa@gmail.com>
2026-01-16 21:32:05 -0600

    [FEAT] Cambios en PathVariableController
    -  importando el decorador Value para leer valores del application.properties
    - definimos el decorador Value para leer la propiedad myname
    - definimos la propiedad privada myname
    - definimos el decorador GetMapping para la solicitud get en donde dimos por nombre a la ruta como "values"
    - creamos el método values en donde devolveremos un diccionario con claves strings y valores como objetos
    - creamos una instancia llamada json para crear un diccionario de datos
    - usamos el método put y definimos la clave myname y pasamos el valor de myname del application proporties
    - devolvemos json

--- src/main/java/com/rinel/curso/springboot/springboot_mvc/controllers/PathVariableController.java
+++ src/main/java/com/rinel/curso/springboot/springboot_mvc/controllers/PathVariableController.java
@@ -9,6 +9,7 @@ import com.rinel.curso.springboot.springboot_mvc.model.dto.Paramdto;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/api/path")
 public class PathVariableController {
+
+    @Value("${config.myname}")
+    private String myname;
 
     @GetMapping("/var/{message}")
     public Paramdto var(@PathVariable String message) {
@@ -40,6 +44,15 @@ public class PathVariableController {
     public User create(@RequestBody User user) {
         return user;
     }
+
+    @GetMapping("/values")
+    public Map<String, Object> values() {
+        Map<String,Object> json = new HashMap<>();
+
+        json.put("myname",myname);
+        return json;
+    }
+    
     
     
     
