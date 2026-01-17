e24bd13413829515fac84db0b2d9ce581a1f3671
Rinel <rineliniguezsosa@gmail.com>
2026-01-16 19:23:43 -0600

    [FEAT] Cambios en PathVariableController
    - importando HashMap
    - importando Map
    - definiendo GetMapping para definir una solicitud http GET con la ruta "multiple" y los parametros de URL product y id
    - creando el método multiple el cual devuelve un diccionario de datod de clave string y valor como un objeto
    - el método multiple acepta 2 argumentos el product y el id
    - creamos un diccionario de datos llamado json y después mediante el método put agregamos datos al diccionario y añadimos los objetos que llegan por la URL
    - devolvemos json

--- src/main/java/com/rinel/curso/springboot/springboot_mvc/controllers/PathVariableController.java
+++ src/main/java/com/rinel/curso/springboot/springboot_mvc/controllers/PathVariableController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.rinel.curso.springboot.springboot_mvc.model.dto.Paramdto;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -19,6 +22,16 @@ public class PathVariableController {
         param.setMessage(message);
         return param;
     }
+
+    @GetMapping("/multiple/{product}/{id}")
+    public Map<String, Object> multiple(@PathVariable String product,@PathVariable Long id) {
+        Map<String,Object> json = new HashMap<>();
+        json.put("product", product);
+        json.put("id", id);
+
+        return json;
+    }
+    
     
     
 }
