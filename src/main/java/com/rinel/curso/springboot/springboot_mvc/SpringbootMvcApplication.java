eef0c935c11f726be2fe80b78713700260c452aa
Rinel <rineliniguezsosa@gmail.com>
2026-01-17 19:19:07 -0600

    [FEAT] Leyendo el archivo values.properties
    - importando PropertySource
    - estableciendo el decorador PropertySource para leer el archivo values.properties

--- src/main/java/com/rinel/curso/springboot/springboot_mvc/SpringbootMvcApplication.java
+++ src/main/java/com/rinel/curso/springboot/springboot_mvc/SpringbootMvcApplication.java
@@ -2,8 +2,10 @@ package com.rinel.curso.springboot.springboot_mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:values.properties")
 public class SpringbootMvcApplication {
 
 	public static void main(String[] args) {
